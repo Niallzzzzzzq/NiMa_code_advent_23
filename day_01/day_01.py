c6adaa4c402eadc684286d8f518cb2497334f311
Niall Maguire <niall.maguire@stryker.com>
2023-12-01 10:37:50 +0000

    first part of day one succesful

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -5,6 +5,16 @@ fp_puzzle_input=os.path.abspath(os.path.join(os.path.dirname(__file__),fp_puzzle
 assert os.path.exists(fp_puzzle_input)
 
 with open(fp_puzzle_input) as fid:
-    puzzle_input=fid.read()
+    puzzle_input=fid.readlines()
 
-print(puzzle_input)
+# print(puzzle_input)
+
+def parse_line(text_line):
+    number_entries=re.findall(r"\d",text_line)
+
+    return int(number_entries[0]+number_entries[-1])
+
+# print(puzzle_input[3])
+# print(parse_line(puzzle_input[3]))
+
+print(sum([parse_line(i) for i in puzzle_input]))
